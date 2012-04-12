f89bc579733447ea85177fb73a1139e76bab1a3e
Taher <odeh@student.gu.se>
2012-04-12 20:41:05 +0200

    added hash_destroy, integration file hashes a pak file...

--- src/integration.c
+++ src/integration.c
@@ -1,5 +1,35 @@
 #include "header.h"
+#include "hash.h"
+#include <stdlib.h>
 
-int main(void){
+int bundle_start(char *pakFile){
+  int ret;
 
+  if ((ret=hash_init(pakFile)) == 1){
+    printf("%s Hashed....\n", pakFile);
+  } else {
+    printf("Filed hashing %s, quitting...\n", pakFile); 
+    return -1;
+  }
+  
+  // map file...
+
+  return 1;
 }
+
+int bundle_stop(){
+  hash_destroy();
+}
+
+int main(int argc, char **argv){
+
+  
+  char *filename = "test.pak";
+
+  bundle_start(filename);
+  
+  return 0;
+}
+
+
+
