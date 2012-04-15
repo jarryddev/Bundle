02aa615ad524cd20b143fc16efe2b23ef47803f1
Taher <odeh@student.gu.se>
2012-04-16 00:11:01 +0200

    added rule: make integrate, segmentation fault

--- src/integration.c
+++ src/integration.c
@@ -3,60 +3,63 @@
 #include "hash.h"
 #include "BundleMemoryMapping.h"
 
-/* 	
-*	compile using:
-* 	gcc integration.c -o ../bin/integration -L../lib -lheader -lhash
-*/
+/*
+ *      compile using:
+ *      gcc integration.c -o ../bin/integration -L../lib -lheader -lhash
+ */
 
 int bundle_start(char *pakFile, struct mappedData *mData){
-  	int ret;
+  int ret;
 
-	// hash file
- 	if ((ret=hash_init(pakFile)) == 1){
+  // hash file
+  if ((ret=hash_init(pakFile)) == 1){
     printf("%s Hashed....\n", pakFile);
-  	} else {
-    	printf("Filed hashing %s, quitting...\n", pakFile); 
-    	return -1;
-  	}
-  
-  	// map file...
-	char *mappedFile = mapPakFile(pakFile, 0, mData);
-	if(mappedFile == NULL)
-	{
-		printf("Failed to map file %s", pakFile);
-		return -1;
-	}
-  	return 1;
+  } else {
+    printf("Filed hashing %s, quitting...\n", pakFile);
+    return -1;
+  }
+
+  // map file...
+  char *mappedFile = mapPakFile(pakFile, 0, mData);
+  if(mappedFile == NULL)
+    {
+      printf("Failed to map file %s", pakFile);
+      return -1;
+    }
+  return 1;
+
 }
 
-void bundle_getIndexDataFor(char *fileName)
+offset_p bundle_getIndexDataFor(char *fileName)
 {
-	// TO DO:
-	// return the index data
-	// offset and size
-	
+  return get_offset(fileName);
 }
 
 int bundle_stop(struct mappedData *mData){
-  	hash_destroy();
-	unMapPakFile (mData->mappedAdress, mData->fileSize);
+  hash_destroy();
+  unMapPakFile (mData->mappedAddress, mData->fileSize);
 }
 
 // This will be done within an objective-C file
 int main(int argc, char **argv){
 
-  	char *filename = "test.pak";
-	struct mappedData *mData;
-	
-  	bundle_start(filename, mData);
+  char *filename = "test.pak";
+  struct mappedData *mData;
 
-	/* Run Game Code From here */
-	
-	/* When done stop Bundle 
-	*	bundle_stop(mData);
-  	*/
+  bundle_start(filename, mData);
 
-  	return 0;
+  //  get test.jpg offset and print it
+  //    offset_p offs = get_offset("test.jpg"); //bundle_getIndexDataFor("test.jpg");
+    //  print_offset(offs);
+  //  free(offs);
+  
+  /* Run Game Code From here */
+
+  /* When done stop Bundle
+//       bundle_stop(mData);
+   */
+
+  return 0;
 }
 
 
