0f6375a699b89fa826b170d630131281dd10bfa0
Taher <odeh@student.gu.se>
2012-05-11 15:48:32 +0200

    ..

--- src/header_main.c
+++ src/header_main.c
@@ -11,18 +11,20 @@ int main(void){
   header_init(fh,3);
 
   //  header_update_head(fh, 12);
-  offset_p offp1 = malloc(sizeof(header_offset));
+  offset_p offp1 = malloc(HEADER_OFFSET_SIZE);
   offp1->hash = 0xFFFFFFFC;
   offp1->size = 0xBBBBBBBBBBBBBBBC;
-  //  offp1->offset_start= 0xCCCCCCCCCCCCCCCC;
-  offset_p offp2 = malloc(sizeof(header_offset));
-  offp2->hash = 0xAAAAAAAA;
-  offp2->size = 0xDDDDDDDDDDDDDDDD;
+  offp1->offset_start= 0xCCCCCCCCCCCCCCCC;
+  offset_p offp2 = malloc(HEADER_OFFSET_SIZE);
+  offp2->hash = 0xAAAAAAAC;
+  offp2->size = 0xDDDDDDDDDDDDDDDC;
   offp2->offset_start= 0xEEEEEEEEEEEEEEEE;
 
-  offset_p offp3 = malloc(sizeof(header_offset));
-  offp3->hash = 0x23232;
-  offp3->size = 0xDDAADD;
+  offset_p offp3 = malloc(HEADER_OFFSET_SIZE);
+  offp2->hash = 0xAAAAAAfC;
+  offp2->size = 0xDDDDDDDDDDDDDDfC;
+  offp2->offset_start= 0xEEEEEEEEEfEEEEEE;
+
 
   header_write_offset(fh, offp1,0);
   header_write_offset(fh, offp2,1);
@@ -31,16 +33,18 @@ int main(void){
 
   free(offp1); free(offp2); free(offp3);
 
+
   unsigned int n_files = 0;
   n_files= header_get_head(fh);
   fprintf(stderr, "Number of files: %d\n", n_files);
 
 
-  //  offset_p offp = header_read_offset(fh, 0xAAAAAAAA);
-  //  print_offset(offp);
-
   offset_p* offx;
   offx= header_get_offsets(fh);
+  int i=0;
+  for (i=0; i< n_files; i++){
+    print_offset(offx[i]);
+  }
   
   free(offx);
   
