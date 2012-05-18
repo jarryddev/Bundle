4be87f253a20fd4125933f16fa9644590ec41b6d
Jarryd Hall <jarrydiosdev@gmail.com>
2012-05-18 23:55:43 +0200

    Re-Indented code

--- src/BundleCheckFileType.c
+++ src/BundleCheckFileType.c
@@ -1,33 +1,33 @@
 /*
-
-Copyright (c) <2012>, <Jarryd Hall, Taher Odeh>
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met: 
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer. 
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution. 
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those
-of the authors and should not be interpreted as representing official policies, 
-either expressed or implied, of the FreeBSD Project.
-
-*/
+ 
+ Copyright (c) <2012>, <Jarryd Hall, Taher Odeh>
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies, 
+ either expressed or implied, of the FreeBSD Project.
+ 
+ */
 
 #include "BundleCheckFileType.h"
 
@@ -78,23 +78,23 @@ int shouldCompressFileType(char *filePath, char *extensions[], int extCount)
 	}
 	
 	/* NOTE: Use this for hard coding the file extensions to compress
-	//Add extension checks here:
-	// possibly only files that need to be compressed
-	
-	// Example ->
-	// if(strcmp("txt", extension) == 0)
-	// 	{
-	// 		printf("txt file\n");	
-	// 		shouldCompress = 1;
-	// 	}
-	
-	// add more file types here
-	
-	else // if file not here, dont compress
-	{
-		shouldCompress = 0;
-	}	
-	*/
+     //Add extension checks here:
+     // possibly only files that need to be compressed
+     
+     // Example ->
+     // if(strcmp("txt", extension) == 0)
+     // 	{
+     // 		printf("txt file\n");	
+     // 		shouldCompress = 1;
+     // 	}
+     
+     // add more file types here
+     
+     else // if file not here, dont compress
+     {
+     shouldCompress = 0;
+     }	
+     */
 	
 	free(tmpExt);
 	extension = NULL;
@@ -103,22 +103,22 @@ int shouldCompressFileType(char *filePath, char *extensions[], int extCount)
 
 /* usage example */
 /*
-int main(void)
-{	
-	int shouldCompressBool;
-	shouldCompressBool = shouldCompressFileType("test/test.png");
-	switch(shouldCompressBool)
-	{
-		case 0:
-			printf("don't compress\n");
-			break;
-		case 1:
-			printf("compress\n");
-			break;
-		default:
-			printf("default hit is switch\n");
-			break;
-	}
-	return 1;
-}
-*/
+ int main(void)
+ {	
+ int shouldCompressBool;
+ shouldCompressBool = shouldCompressFileType("test/test.png");
+ switch(shouldCompressBool)
+ {
+ case 0:
+ printf("don't compress\n");
+ break;
+ case 1:
+ printf("compress\n");
+ break;
+ default:
+ printf("default hit is switch\n");
+ break;
+ }
+ return 1;
+ }
+ */
--- src/BundleMemoryMapping.c
+++ src/BundleMemoryMapping.c
@@ -1,33 +1,33 @@
 /*
-
-Copyright (c) <2012>, <Jarryd Hall, Taher Odeh>
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met: 
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer. 
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution. 
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those
-of the authors and should not be interpreted as representing official policies, 
-either expressed or implied, of the FreeBSD Project.
-
-*/
+ 
+ Copyright (c) <2012>, <Jarryd Hall, Taher Odeh>
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies, 
+ either expressed or implied, of the FreeBSD Project.
+ 
+ */
 
 // BundleMemoryMapping.c
 // author: Jarryd Hall
@@ -75,29 +75,29 @@ size_t size;
 
 // this maps the pak file
 int mapPakFile(const char *fileToOpen, long startOffset, struct mappedData *mData){
-
+    
   	FILE *file = fopen(fileToOpen, "rb");
-		if(file != NULL)
-		{
-			fseek(file, 0, SEEK_END);
+    if(file != NULL)
+    {
+        fseek(file, 0, SEEK_END);
 	  	mData->fileSize = ftell(file);
 	  	fseek(file, 0, SEEK_SET);
 	  	printf("File size is %lu bytes.\n", (unsigned long) mData->fileSize);
-
+        
 	  	int fileDescriptor = fileno(file);
 	  	off_t offset = startOffset;
-
+        
 	  	// map the data
 	  	mData->mappedAddress = mmap(NULL, mData->fileSize, PROT_READ, MAP_PRIVATE, fileDescriptor, offset);
 	  	if(mData->mappedAddress == (void *) -1)
-	    	{
+        {
 	      	printf("Failed to map file to virtual memory.\n");
 	      	fclose(file);
 	      	return 1;
-	    	}
+        }
 	  	printf("Successfully mapped file to virtual memory.\n");
 	  	printf("Starting Mapped Address is: %X \n", (char *) mData->mappedAddress);
-
+        
 	  	// use this for optimizing the kernel for our intended use of the mapped data
 	  	int ret;
 	  	// tell kernel how we use data from addr -> addr + len
@@ -105,76 +105,76 @@ int mapPakFile(const char *fileToOpen, long startOffset, struct mappedData *mDat
 	  	ret = madvise (mData->mappedAddress, mData->fileSize, MADV_RANDOM);
 	  	if (ret < 0)
 	    	perror ("madvise");
-
+        
 	  	fclose(file);
 	  	//printf("Reading %lu bytes.\n", (unsigned long)fileSize);
 	  	return 0;
-		}
-		// file couldnt be opened
-		return 1;
+    }
+    // file couldnt be opened
+    return 1;
   	
 }
 
 int unMapPakFile (void *mappedAddress, size_t fileSize){
-  if(munmap(mappedAddress, fileSize) != 0)
+    if(munmap(mappedAddress, fileSize) != 0)
     {
-      printf("Failed to unmap file.\n");
+        printf("Failed to unmap file.\n");
         return -1;
     }
-  else
+    else
     {
-      printf("Successfully unmapped file with size %lu.\n", (unsigned long)fileSize);
+        printf("Successfully unmapped file with size %lu.\n", (unsigned long)fileSize);
         return 0;
     }
 }
 
 unsigned char * getDataForOffsets(void *mappedAddress, size_t startOffset, size_t numberOfBtyes){
-  unsigned char *temp;
-  temp = (unsigned char*)  mappedAddress+startOffset;
-
-  printf("Reading %lu bytes.\n", (unsigned long)numberOfBtyes);
-
-  //	* EXAMPLE USAGE:
-  FILE *sampleFile;
-  sampleFile = fopen("Sample.png", "wb");
-  printf("number of bytes: %ld, strt offset: %lu\n vmap address: %lu, start offset: %lu\n", numberOfBtyes, startOffset,mappedAddress,startOffset);
-  size_t i;
-  for(i = startOffset; i < (startOffset + numberOfBtyes)/2; i++)
+    unsigned char *temp;
+    temp = (unsigned char*)  mappedAddress+startOffset;
+    
+    printf("Reading %lu bytes.\n", (unsigned long)numberOfBtyes);
+    
+    //	* EXAMPLE USAGE:
+    FILE *sampleFile;
+    sampleFile = fopen("Sample.png", "wb");
+    printf("number of bytes: %ld, strt offset: %lu\n vmap address: %lu, start offset: %lu\n", numberOfBtyes, startOffset,mappedAddress,startOffset);
+    size_t i;
+    for(i = startOffset; i < (startOffset + numberOfBtyes)/2; i++)
     {
-      fwrite(temp, 1, 1, sampleFile);
-      printf("Address [%02lu] : %X = %02X \n", (unsigned long)i, temp, *(temp));
-      temp++;
+        fwrite(temp, 1, 1, sampleFile);
+        printf("Address [%02lu] : %X = %02X \n", (unsigned long)i, temp, *(temp));
+        temp++;
     }
-  fclose(sampleFile);
-
-  return temp;
+    fclose(sampleFile);
+    
+    return temp;
 }
 
 /*
-int main (){
-
-  FILE *f;
-  int tempSize = 0;
-  if (f = fopen("test.jpg", "rb"))
-    {
-      fseek(f, 0, SEEK_END);
-      size = ftell(f);
-      tempSize = size;
-      printf("File size of file to map is %lu\n", (unsigned long) size);
-      printf("Temp File size of file to map is %lu\n", (unsigned long) tempSize);
-      fclose(f);
-    }
-
-  struct mappedData m;
-
-  char *mappedFile = mapPakFile("test.jpg", 0, &m);
-
-  //sleep(5);
-  printf("Size to be passed to getDataForOffsets is %lu\n", (unsigned long) tempSize);
-  printf("Test is %lu\n", (unsigned long) (size));
-  unsigned char *fetchedData = getDataForOffsets(mappedFile, 0, size);
-  unMapPakFile(mappedFile, size);
-
-  return 0;
-}
-*/
+ int main (){
+ 
+ FILE *f;
+ int tempSize = 0;
+ if (f = fopen("test.jpg", "rb"))
+ {
+ fseek(f, 0, SEEK_END);
+ size = ftell(f);
+ tempSize = size;
+ printf("File size of file to map is %lu\n", (unsigned long) size);
+ printf("Temp File size of file to map is %lu\n", (unsigned long) tempSize);
+ fclose(f);
+ }
+ 
+ struct mappedData m;
+ 
+ char *mappedFile = mapPakFile("test.jpg", 0, &m);
+ 
+ //sleep(5);
+ printf("Size to be passed to getDataForOffsets is %lu\n", (unsigned long) tempSize);
+ printf("Test is %lu\n", (unsigned long) (size));
+ unsigned char *fetchedData = getDataForOffsets(mappedFile, 0, size);
+ unMapPakFile(mappedFile, size);
+ 
+ return 0;
+ }
+ */
