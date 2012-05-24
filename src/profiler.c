9e4606e63759c98ac7cd857673c82c355a6e2538
Taher <odeh@student.gu.se>
2012-05-24 20:00:13 +0200

    profiler. see profiler.c for usage

---
+++ src/profiler.c
@@ -0,0 +1,35 @@
+#include <sys/time.h>
+#include <stdio.h>
+struct timeval tv;
+
+int profiler_start();
+suseconds_t profiler_getTime();
+
+long long doSum(long long  a){
+  if (a<=1) return 1L;
+  return a+doSum(a-1);
+}
+
+/**
+ * Usage example 
+ *
+ */
+int main(){
+  
+  if (profiler_start()){
+    int sum=doSum(100000L);
+    printf("Call: doSum(), time: %lu\n", profiler_getTime());
+  }
+}
+
+
+int profiler_start(){
+  return (gettimeofday(&tv,NULL)==0);
+}
+
+suseconds_t profiler_getTime(){
+  suseconds_t usec= tv.tv_usec;
+  gettimeofday(&tv,NULL);
+  return tv.tv_usec-usec;
+}
+
